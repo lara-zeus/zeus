5aa81fa427404119002a193aef6a69c0469dcc19
atmonshi <atmonshi@users.noreply.github.com>
2023-06-21 05:09:56 +0000

    Fix styling

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::redirect('/','/blog');
+Route::redirect('/', '/blog');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
