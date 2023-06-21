3479ec2d3f2c7c361e39357280fca65924f571dd
Ash Monsh <wh7r.com@gmail.com>
2023-06-21 05:00:32 +0300

    clean up

--- routes/web.php
+++ routes/web.php
@@ -14,10 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-/*Route::get('/', function () {
-    return view('welcome');
-});*/
-
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
