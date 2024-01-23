269eccc2849eb4f1be6d4ffbacebdc0252fd69af
Ash Monsh <wh7r.com@gmail.com>
2024-01-23 18:28:30 +0300

    pint

--- database/migrations/2024_01_23_150122_create_navigations_table.php
+++ database/migrations/2024_01_23_150122_create_navigations_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
      */
     public function up()
     {
-        if (!Schema::hasTable('navigations')) {
+        if (! Schema::hasTable('navigations')) {
             Schema::create('navigations', function (Blueprint $table) {
                 $table->id();
                 $table->string('name');
