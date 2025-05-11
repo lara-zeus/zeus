fbb581efaaba9c0e72da58000f0e14013131041c
Ash Monsh <wh7r.com@gmail.com>
2025-05-11 16:11:06 +0300

    trigger test

--- app/View/Components/AppLayout.php
+++ app/View/Components/AppLayout.php
@@ -12,6 +12,7 @@ class AppLayout extends Component
      */
     public function render(): View
     {
+        //
         return view('layouts.app');
     }
 }
--- tests/CreatesApplication.php
+++ tests/CreatesApplication.php
@@ -12,7 +12,6 @@ trait CreatesApplication
      */
     public function createApplication(): Application
     {
-        //
         $app = require __DIR__.'/../bootstrap/app.php';
 
         $app->make(Kernel::class)->bootstrap();
