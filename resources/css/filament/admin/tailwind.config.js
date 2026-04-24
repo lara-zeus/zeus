8211811ef728ec84f7c12a2eb69a74395cfd7151
Ash Monsh <wh7r.com@gmail.com>
2026-04-25 02:10:20 +0300

    wip

--- resources/css/filament/admin/tailwind.config.js
+++ resources/css/filament/admin/tailwind.config.js
@@ -1,7 +1,5 @@
-import preset from '../../../../vendor/filament/filament/tailwind.config.preset'
 
 export default {
-    presets: [preset],
     content: [
         './app/Filament/**/*.php',
         './resources/views/filament/**/*.blade.php',
